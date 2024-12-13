ba8666e4a0af7a16edfa093df6421744c23d166d
Nolan0027 <128444566+Nolan0027@users.noreply.github.com>
2024-12-13 17:10:38 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,21 +1,36 @@
 import os
-import random
 import discord
 from discord.ext import commands
+
+# Import commands dynamically
+from bot.commands.ping import ping
+from bot.commands.move import move
 import time
 
-# Retrieve the token from environment variable
+# Define token
 TOKEN = os.environ.get('TOKEN')
 
 if not TOKEN:
     raise ValueError("The TOKEN environment variable is not set!")
 
-# Intents configuration for the bot
+bot_start_time = time.time()
+
+# Bot setup
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
-# Properly create the bot instance
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+# User data
+bot.user_positions = {}
+
+# Register commands
+bot.command(name='ping')(ping)
+bot.command(name='move')(move)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is online.')
 
 bot.run(TOKEN)
